e0832267943f137e0ddbd887238117cecc00d2fb
aguilanbon <aguilanbon@gmail.com>
2022-05-26 13:02:38 +0800

    updated links for deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ function Header() {
   return (
     <div>
       <h1>
-          <Link to="/">
-            <span style={{ color: '#16b882' }}>b</span>movies
-          </Link>
+        <Link to="/movie-search-react">
+          <span style={{ color: '#16b882' }}>b</span>movies
+        </Link>
       </h1>
       <footer>TMDB api - Movie Search</footer>
     </div>
